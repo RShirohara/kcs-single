6df08681f2c3b1e01e6d020282477618c3430ca9
Ray Shirohara <47940764+RShirohara@users.noreply.github.com>
2019-12-21 17:43:29 +0900

    First commit

---
+++ script/03.random_shigure.py
@@ -0,0 +1,16 @@
+#! usr/bin/python
+# -*- coding: utf-8 -*-
+# author: Shiro_Shz
+
+import os
+import random
+import sys
+
+args = sys.argv[1]
+
+with open("Novel/kcs/Single/data/03/selected21_shigure.dat", 'r') as _file:
+    _dat = _file.readlines()
+
+for _cnt in range(int(args)):
+    _loadcnt = random.randint(0, len(_dat))
+    print(_dat[_loadcnt].replace("\n", ""))
